21a48d19e38c300b9afd28491449c9c8fd7e2a30
ronejfourn <rs2102@protonmail.com>
2022-12-03 20:02:37 +0545

    move process_image into app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
-from process_image import process_image
 import onnxruntime as ort
 import numpy as np
+from PIL import Image, ImageOps
 
 app = Flask(__name__)
 
@@ -9,7 +9,14 @@ sess = ort.InferenceSession("mnist-8.onnx")
 input_name = sess.get_inputs()[0].name
 label_name = sess.get_outputs()[0].name
 
-def indexof_max(arr):
+def process_image(img):
+    img = Image.open(img)
+    img = img.convert('L')
+    img = ImageOps.invert(img)
+    img = img.resize((28, 28))
+    return img
+
+def index_of_max(arr):
     m = 0
     for i in range(len(arr)):
         if (arr[i] > arr[m]):
@@ -23,7 +30,7 @@ def predict():
     arr = (np.array(img) / 255).astype(np.float32)
     arr = np.reshape(arr, (1, 1, 28, 28))
     prediction = sess.run([label_name], {input_name: arr})[0]
-    output = indexof_max(prediction[0])
+    output = index_of_max(prediction[0])
     return str(output)
 
 if __name__ == '__main__':
